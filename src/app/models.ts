a8e79d09c1e7c967b73751fdd8839a535f8b4313
JUAN DAVID PARRA PATIÑO <a181317@cm.es>
2021-04-29 22:09:31 +0200

    created services and interceptors

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,5 +1,5 @@
 export interface Game {
-    backgroung_image: string;
+    background_image: string;
     name: string;
     released: string;
     metacritic_url: string;
@@ -7,4 +7,43 @@ export interface Game {
     description: string;
     metacritic: number;
     genres: Array<Genre>;
+    parent_platforms: Array<ParentPlatform>;
+    publishers: Array<Publishers>;
+    ratings: Array<Rating>;
+    screenshots: Array<Screenshots>;
+    trailers: Array<Trailer>;
+}
+
+export interface APIResponse<T> {
+    results: Array<T>;
+}
+
+interface Genre {
+    name: string;
+}
+
+interface ParentPlatform {
+    platform: {
+        name: string;
+    };
+}
+
+interface Publishers {
+    name: string;
+}
+
+interface Rating {
+    id: number;
+    count: number;
+    title: string;
+}
+
+interface Screenshots {
+    image: string;
+}
+
+interface Trailer {
+    data: {
+        max: string;
+    };
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment as env } from 'src/environments/environment';
+import { APIResponse, Game } from '../models';
 
 @Injectable({
   providedIn: 'root'
@@ -21,7 +22,7 @@ export class HttpService {
     }
 
     return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
-      params: params,
+      params
     });
   }
 }
