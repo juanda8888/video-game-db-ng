26337c41eefad0cf5a1e1a984a32ee2b5e229dab
JUAN DAVID PARRA PATIÑO <jparrapa@externos.bankia.com>
2021-04-29 17:18:50 +0200

    creando las interfaces

---
+++ src/app/models.ts
@@ -0,0 +1,10 @@
+export interface Game {
+    backgroung_image: string;
+    name: string;
+    released: string;
+    metacritic_url: string;
+    website: string;
+    description: string;
+    metacritic: number;
+    genres: Array<Genre>;
+}
